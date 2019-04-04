4d1b4dc3febf51b7673ffb753948dff272a2a43b
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-03 19:58:10 -1000

    Refactored Color Swatch code, added ability to draw squares from top-left to bottom-right

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,8 +1,6 @@
-let pixelPainter = function(width, height){
-  const fontAwesomeLink = document.createElement("link");
-  fontAwesomeLink.href = "https://use.fontawesome.com/releases/v5.8.1/css/all.css";
-  document.getElementsByTagName("head")[0].appendChild(fontAwesomeLink);
-  
+let pixelPainter = (function(){
+  let width = 30;
+  let height = 30;
   
   const pixelPainterDiv= document.getElementById('pixelPainter');
   const myColors = ['red', 'green', 'blue', 'yellow', 'orange',
@@ -11,74 +9,80 @@ let pixelPainter = function(width, height){
   let selectedColor;
   let mouseButtonIsDown = false;
   let drawBoxShape = false;
+  let drawBoxShapeStep = 1;
+  let drawBoxShapeStart;
+  let drawBoxShapeEnd;
 
-  function colorSwatch() {
-    const colorSwatch = document.createElement("div");
-    colorSwatch.id = 'color-swatch';
-    pixelPainterDiv.appendChild(colorSwatch);
-    const choiceDiv = document.createElement("div");
-    choiceDiv. id = 'swatch-colors';
-    colorSwatch.appendChild(choiceDiv);
+  
+  
 
-    for (let divCount = 0; divCount < myColors.length; divCount++){
-      let colorSwatchDiv = document.createElement("div");
-      colorSwatchDiv.className = 'color-choice'
-      colorSwatchDiv.style.background = myColors[divCount];
-      colorSwatchDiv.addEventListener('click', function(){
+  const colorSwatch = (function (){
+    const colorSwatchDiv = document.createElement("div");
+    pixelPainterDiv.appendChild(colorSwatchDiv);
+    colorSwatchDiv.id = 'color-swatch';
+
+    const colorSelection = document.createElement("div");
+    colorSwatchDiv.appendChild(colorSelection);
+    colorSelection. id = 'swatch-colors';
+
+    let totalColors = myColors.length + randomColorCount;
+    for (let i = 0; i < totalColors; i++){
+      let color = document.createElement("div");
+      colorSelection.appendChild(color);
+      color.addEventListener('click', function(){
         selectedColor = this.style.background;
-      })
-      choiceDiv.appendChild(colorSwatchDiv);
-    }
-
-    for (divCount = 1; divCount < randomColorCount; divCount++){
-      colorSwatchDiv = document.createElement("div");
-      colorSwatchDiv.className = 'color-choice';
+      });
+      color.className = 'color-choice';
+      if (i < myColors.length){
+        color.style.background = myColors[i];
+        console.log('reached');
+        continue;
+      }
       let rValue = Math.floor(Math.random() * 255);
       let gValue = Math.floor(Math.random() * 255);
       let bValue = Math.floor(Math.random() * 255);
-      colorSwatchDiv.style.background = 'rgb(' + rValue.toString() + ', ' + gValue.toString() + ' ,' + bValue.toString() + ')';
-      colorSwatchDiv.addEventListener('click', function(){
-        selectedColor = this.style.background;
-      })
-      choiceDiv.appendChild(colorSwatchDiv);
+      color.style.background = 'rgb(' + rValue.toString() + ', ' + gValue.toString() + ' ,' + bValue.toString() + ')';
     }
     
-    let buttonDiv = document.createElement("button-div");
-    buttonDiv.id = "swatch-buttons";
-    colorSwatch.appendChild(buttonDiv);
-    let buttonElement = document.createElement("button");
-    buttonDiv.appendChild(buttonElement);
-    buttonElement.className = "swatch-button";
-    buttonElement.innerHTML = "Clear";
-    buttonElement.addEventListener('click', function(){
+    const buttonSelection = document.createElement("div");
+    colorSwatchDiv.appendChild(buttonSelection);
+    buttonSelection.id = "swatch-buttons";
+    
+    const clear = document.createElement("button");
+    buttonSelection.appendChild(clear);
+    clear.addEventListener('click', function(){
       const allCanvasCells = document.getElementsByClassName('canvas-cell');
       for (let currentCanvasCell = 0; currentCanvasCell < allCanvasCells.length; currentCanvasCell++){
         allCanvasCells[currentCanvasCell].style.background = 'transparent';
       }
-    })
-
-    buttonElement = document.createElement("button");
-    buttonDiv.appendChild(buttonElement);
-    buttonElement.className = "swatch-button";
-    buttonElement.innerHTML = "Erase";
-    buttonElement.addEventListener('click', function(){
+    });
+    clear.className = "swatch-button";
+    clear.innerHTML = "Clear";
+    
+    const erase = document.createElement("button");
+    buttonSelection.appendChild(erase);
+    erase.addEventListener('click', function(){
       selectedColor = 'transparent';
-    })
-  }
+    });
+    erase.className = "swatch-button";
+    erase.innerHTML = "Erase";
+    
+    const drawBoxModule = (function(){
+      const moduleButton = document.createElement('button');
+      moduleButton.className = 'swatch-button';
+      moduleButton.textContent = 'Draw Box';
+      buttonSelection.appendChild(moduleButton);
+      
+      moduleButton.addEventListener('click', function(){
+        drawBoxShape = !drawBoxShape;
+        console.log(drawBoxShape);
+      });
+    })();
+  })();
 
-  // let boxShape = function(){
-  //   const boxShapeButton = document.createElement('button');
-  //   let myState = false;
-
-  //   boxShapeButton.className = 'swatch-button';
-  //   boxShapeButton.textContent = 'Draw Boxes: OFF';
-  //   pixelPainterDiv.appendChild(boxShapeButton);
-  //   boxShapeButton.addEventListener('click', function(){
-  //     myState = !myState;
-  //   })
-  // }();
   
-  function canvas(){
+  
+  const canvas = function(){
     const canvasDiv = document.createElement("div");
     pixelPainterDiv.appendChild(canvasDiv);
     canvasDiv.id = "canvas-container";
@@ -103,6 +107,20 @@ let pixelPainter = function(width, height){
         }
         cellElement.onmousedown = function(){
           mouseButtonIsDown = true;
+          if (drawBoxShape && drawBoxShapeStep === 1){
+            drawBoxShapeStart = this;
+          }
+          if (drawBoxShape && drawBoxShapeStep === 2){
+            drawBoxShapeEnd = this;
+            for (let i = drawBoxShapeStart.cellIndex; i < drawBoxShapeEnd.cellIndex; i++){
+              canvas.rows[drawBoxShapeEnd.parentElement.rowIndex].cells[i].style.background = selectedColor;
+              canvas.rows[drawBoxShapeStart.parentElement.rowIndex].cells[i].style.background = selectedColor;  
+            }
+            for (let i = drawBoxShapeStart.parentElement.rowIndex; i < drawBoxShapeEnd.parentElement.rowIndex; i++){
+              canvas.rows[i].cells[drawBoxShapeStart.cellIndex].style.background = selectedColor;
+              canvas.rows[i].cells[drawBoxShapeEnd.cellIndex].style.background = selectedColor;
+            }
+          }
           this.style.background = selectedColor;
         }
         cellElement.onmouseenter = function(){
@@ -112,17 +130,20 @@ let pixelPainter = function(width, height){
         }
         cellElement.onmouseup = function(){
           mouseButtonIsDown = false;
+          if (drawBoxShape){
+            drawBoxShapeStep++;
+          }
+          if (drawBoxShapeStep > 2){
+            drawBoxShapeStep = 1;
+          }
         }
       }
     }
-  }
+  }();
 
   return {
-    colorSwatch,
-    canvas,
+    width,
+    height,
   }
-}
+})();
 
-myPixelPainter = pixelPainter(30, 30);
-myPixelPainter.colorSwatch();
-myPixelPainter.canvas();
